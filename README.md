a0878edd3916f8d9e45b92adc5373da33d3cf0f6
Kieran Browne <kierantbrowne@gmail.com>
2017-09-02 04:38:28 +1000

    initial

---
+++ README.md
@@ -0,0 +1,14 @@
+# Auto Differentiation
+
+An attempt at auto diff.
+
+## Usage
+
+FIXME
+
+## License
+
+Copyright © 2017 FIXME
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ src/autodiff/core.clj
@@ -0,0 +1,41 @@
+(ns autodiff.core)
+
+(defprotocol AutoDiff
+  (+ [u v])
+  (- [u v])
+  (* [u v])
+  )
+
+(defmacro destruct [content]
+  `(let ~[{'u :f 'u' :f' :or {'u 'u 'u' 0}} 'u
+         {'v :f 'v' :f' :or {'v 'v 'v' 0}} 'v]
+     ~content))
+
+
+(defrecord Dual [f f']
+  AutoDiff
+  (+ [u v]
+    (destruct
+      (Dual. (+ u v) (+ u' v'))))
+  (- [u v]
+    (destruct
+      (Dual. (- u v) (- u' v'))))
+  (* [u v]
+    (destruct
+      (Dual. (* u v) (+ (* u' v) (* u v')))))
+  )
+
+(extend-type java.lang.Long
+  AutoDiff
+  (+ [u v] (clojure.core/+ u v))
+  (- [u v] (clojure.core/- u v))
+  (* [u v] (clojure.core/* u v))
+
+    )
+
+
+
+(defn sq [x] (* x (+ x 0)))
+
+(sq (Dual. 3 1))
+(sq 3)
