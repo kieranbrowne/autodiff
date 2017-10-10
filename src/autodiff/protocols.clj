263165a2b9f36036108ba61d642391dc128affd1
Kieran Browne <kierantbrowne@gmail.com>
2017-10-11 00:24:08 +1100

    tweaking matmul deriv

--- src/autodiff/protocols.clj
+++ src/autodiff/protocols.clj
@@ -87,7 +87,7 @@
       (Dual. (mul u v) (add (mul u' v) (mul u v')))))
   (matmul [u v]
     (destruct-binary
-     (Dual. (matmul u v) (add (matmul v' u) (transpose (matmul v u' ))))))
+     (Dual. (matmul u v) (add (matmul u' v) (transpose (matmul u v' ))))))
   (div [u v]
     (destruct-binary
      (Dual. (div u v) (div (sub (mul u' v) (mul u v')) (mul v v)))))
